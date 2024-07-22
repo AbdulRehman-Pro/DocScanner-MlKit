b36a951535749b1de8daf88f81e3101c75083413
Abdul Rehman <rehmankhan8360@gmail.com>
2024-07-22 11:16:15 +0500

    update animation

--- app/src/main/java/com/rehman/docscan/SplashActivity.kt
+++ app/src/main/java/com/rehman/docscan/SplashActivity.kt
@@ -2,24 +2,31 @@ package com.rehman.docscan
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rehman.docscan.ui.ContainerActivity
 import com.rehman.utilities.Utils
+import java.time.Clock
+import java.time.Instant
 
 private const val SPLASH_TIME: Long = 2000
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         this.installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Utils.hideSystemUiVisibility(this)
+
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity( Intent(this, ContainerActivity::class.java))
--- app/src/main/java/com/rehman/docscan/ui/ContainerActivity.kt
+++ app/src/main/java/com/rehman/docscan/ui/ContainerActivity.kt
@@ -1,10 +1,17 @@
 package com.rehman.docscan.ui
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -17,6 +24,9 @@ import com.rehman.utilities.Utils
 
 class ContainerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityContainerBinding
+    private var doubleBackToExitPressedOnce = false
+    private val handler = Handler(Looper.getMainLooper())
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,7 @@ class ContainerActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.homeFragment -> binding.titleAppBar.text = getString(R.string.docscan)
@@ -37,6 +48,32 @@ class ContainerActivity : AppCompatActivity() {
         }
 
 
+        // Handle the back press logic
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (navController.currentDestination?.id == R.id.homeFragment) {
+                    if (doubleBackToExitPressedOnce) {
+                        finish() // or you can call super.onBackPressed() if you want to use the default back behavior
+                    } else {
+                        doubleBackToExitPressedOnce = true
+                        Toast.makeText(this@ContainerActivity, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+
+                        handler.postDelayed({
+                            doubleBackToExitPressedOnce = false
+                        }, 2000) // 2 seconds delay
+                    }
+                } else {
+                    navController.navigateUp() // navigate to the previous fragment in the stack
+                }
+            }
+        })
+
+
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        super.onDestroy()
     }
 
 }
